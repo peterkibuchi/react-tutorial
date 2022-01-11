5b7d93ae9d1ec2440e07db34e57f6da789a3cbbe
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-01-11 06:22:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,10 +29,11 @@ class App extends Component {
             <div className="container">
                 <h1>React Tutorial</h1>
                 <p>Add a character with a name and a job to the table.</p>
-                <Table characterData={characters} removeCharacter={this.removeCharacter} />
 
                 <h3>Add New</h3>
                 <Form handleSubmit={this.handleSubmit} />
+
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
         );
     }
